b1e9eefa8cd37ef3f0f04382d59f97aeb0d2c75e
Igor Elias <igor.elias.muniz@gmail.com>
2017-09-06 01:10:22 -0300

    insert on db

--- app/sender.py
+++ app/sender.py
@@ -1,9 +1,25 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("Registered message!")
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+    register_message(subject, message)
+
     return 'Message inside of queue!<br /> Subject: {}<br /> Message: {}'.format(
         subject, message
     )
